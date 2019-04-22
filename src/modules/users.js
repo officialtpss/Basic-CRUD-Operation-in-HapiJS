102393ec95d901da7a8ceb8a107dc7ccd5d35499
officialtpss <officialtpss@gmail.com>
2019-04-22 14:55:59 +0530

    #update

--- src/modules/users.js
+++ src/modules/users.js
@@ -5,19 +5,19 @@
  */
 
 const mongoose = require('mongoose');
-const {getErrorMessage} = require('./../handler/error.handler');
+const { getErrorMessage } = require('./../handler/error.handler');
 const user = mongoose.model('users');
-const {getAccessToken} = require('./../middleware/jwt');
+const { getAccessToken } = require('./../middleware/jwt');
 
 // create new user
 const create = async (req, reply) => {
   const doc = new user(req.payload);
   try {
     await doc.save();
-    return reply.response({ok: 1}).code(201);
-  } catch(e) {
+    return reply.response({ ok: 1 }).code(201);
+  } catch (e) {
     return reply.response({
-      message : getErrorMessage(e),
+      message: getErrorMessage(e),
     }).code(400);
   }
 };
@@ -25,15 +25,15 @@ const create = async (req, reply) => {
 // login user
 const login = async (req, reply) => {
   try {
-    const data = await user.findOne(req.payload, { email: 1}).exec();
-    if(!data) {
-      return reply.response({message: 'invalid email or password'}).code(400);
-    }else {
-      return {token: await getAccessToken(data.email)};
+    const data = await user.findOne(req.payload, { email: 1 }).exec();
+    if (!data) {
+      return reply.response({ message: 'invalid email or password' }).code(400);
+    } else {
+      return { token: await getAccessToken(data.email) };
     }
-  } catch(e) {
+  } catch (e) {
     return reply.response({
-      message : getErrorMessage(e),
+      message: getErrorMessage(e),
     }).code(400);
   }
 };
@@ -41,11 +41,11 @@ const login = async (req, reply) => {
 // get information of user
 const getUser = async (req, reply) => {
   try {
-    const data = await user.findOne({ _id: req.userId },{ password: 0 }).exec();
+    const data = await user.findOne({ _id: req.userId }, { password: 0 }).exec();
     return data;
-  } catch(e) {
+  } catch (e) {
     return reply.response({
-      message : getErrorMessage(e),
+      message: getErrorMessage(e),
     }).code(400);
   }
 };
@@ -53,11 +53,11 @@ const getUser = async (req, reply) => {
 // update user information
 const updateUser = async (req, reply) => {
   try {
-    await user.updateOne({ _id: req.userId },req.payload).exec();
-    return { ok :1};
-  } catch(e) {
+    await user.updateOne({ _id: req.userId }, req.payload).exec();
+    return { ok: 1 };
+  } catch (e) {
     return reply.response({
-      message : getErrorMessage(e),
+      message: getErrorMessage(e),
     }).code(400);
   }
 };
@@ -66,10 +66,10 @@ const updateUser = async (req, reply) => {
 const deleteUser = async (req, reply) => {
   try {
     await user.deleteOne({ _id: req.userId }).exec();
-    return { ok :1};
-  } catch(e) {
+    return { ok: 1 };
+  } catch (e) {
     return reply.response({
-      message : getErrorMessage(e),
+      message: getErrorMessage(e),
     }).code(400);
   }
 };
